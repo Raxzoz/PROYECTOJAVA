1780f098272d123741eb981138ff4b47a069b14c
Raxzoz <102984649+Raxzoz@users.noreply.github.com>
2022-04-22 20:31:06 -0600

    Add files via upload

---
+++ PROYECTOCOD/PROYECTOCOD/src/proyectocod/Variables.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectocod;
+
+/**
+ *
+ * @author EDUARD VM
+ */
+public class Variables {
+    
+}
